5f0c71eb8891c92a56d3c7e3f539f81c60245a7b
maxikgreat <maximvasylenko228322@gmail.com>
2020-05-28 20:01:48 +0200

    Validation added

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -1,8 +1,48 @@
-import {useState} from 'react';
+import {useState, useCallback} from 'react';
 
 export const useHttp = () => {
     const [loading, setLoading] = useState(false);
-    const request = () => {
+    const [error, setError] = useState(null);
 
-    }
+    const request = useCallback(async (
+        url, 
+        method = 'GET',
+        body = null, 
+        headers = {}
+    ) => {
+        setLoading(true);
+        try {
+            if (body) {
+                body = JSON.stringify(body);
+                headers['Content-Type'] = 'application/json';
+            }
+
+            const response = await fetch(url, {
+                method, body, headers
+            });
+            const data = await response.json();
+
+            if (!response.ok) {
+                //console.log(data.errors);
+
+                const errors = {};
+                data.errors.forEach(error => {
+                    errors[error.param] = error.msg;
+                })
+                throw errors || 'Something went wrong'; // error thrown in catch
+            }
+            setLoading(false);
+
+            return data;
+
+        } catch (e) {
+            setLoading(false);
+            setError(e);
+            throw e;
+        }
+    }, []);
+
+    const clearError = () => setError(null);
+
+    return {loading, error, request, clearError};
 }
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,23 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { useHttp } from '../hooks/http.hook';
 
 export const Auth = () => {
-
+    const {loading, error, request} = useHttp();
     const [form, setForm] = useState({
         email: '',
         password: ''
     });
+
+    useEffect(() => {
+        console.log(error);
+    }, [error])
 
     const changeHandler = event => {
         setForm({
             ...form,
             [event.target.name]: event.target.value
         })
-    }
+    };
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form});
+        } catch (e) {}
+    };
 
     return (
         <div className="row">
             <div className="col s6 offset-s3">
-                <h1>Short link</h1>
+                <h1>Short your link</h1>
                 <div className="card">
                     <div className="card-content white-text">
                         <span className="card-title">Authorization</span>
@@ -26,27 +37,45 @@ export const Auth = () => {
                                 <input 
                                     placeholder="Enter email..." 
                                     id="email" 
-                                    type="text"
+                                    type="email"
                                     name="email"
                                     onChange={changeHandler}
                                 />
                                 <label htmlFor="email">Email</label>
+                                {
+                                    error?.email
+                                    ? <span className="helper-text red-text text-lighten-1">{error.email}</span>
+                                    : null
+                                }
                             </div>
                             <div className="input-field">
                                 <input
                                     placeholder="Enter password..." 
                                     id="password" 
-                                    type="text" 
+                                    type="password" 
                                     name="password"
                                     onChange={changeHandler}
                                 />
                                 <label htmlFor="password">Password</label>
+                                {
+                                    error?.password
+                                    ? <span className="helper-text red-text text-lighten-1">{error.password}</span>
+                                    : null
+                                }
                             </div>
                         </div>
                     </div>
                     <div className="card-action">
-                        <button className="btn yellow darken-4" style={{marginRight: '10px'}}>Log in</button>
-                        <button className="btn grey lighten-1 black-text">Sign up</button>
+                        <button 
+                            className="btn yellow darken-4" 
+                            style={{marginRight: '10px'}}
+                            disabled={loading}
+                        >Log in</button>
+                        <button 
+                            className="btn grey lighten-1 black-text"
+                            disabled={loading}
+                            onClick={registerHandler}
+                        >Sign up</button>
                     </div>
                 </div>
             </div>
